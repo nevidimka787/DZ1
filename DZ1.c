4d88e543b905b0e73f3d6ed2302db0a51269ccae
AlexeyVlasoviu4 <91986603+AlexeyVlasoviu4@users.noreply.github.com>
2021-10-22 09:17:13 +0300

    Add files via upload

--- DZ1.c
+++ DZ1.c
@@ -2,24 +2,23 @@
 #include <windows.h>
 #include <locale.h>
 
-void convertion( int n )
+void convertion(int n)
 {
-if ( n >= 2 )
-{
-convertion( n/2 );
-}
-printf("%d", n % 2);
-}
-
-
-int main (void) {
-setlocale(LC_ALL, "Russian");
-int n;
-printf("Введите десятичное число: \n");
-scanf("%d", &n);
-printf("(Десятичная Cистема счисления): %d = ", n);
-convertion( n );
-printf(" (Двоичная система счисления)\n");
-return 0;
+	if (n >= 2)
+	{
+		convertion(n / 2);
+	}
+	printf("%d", n % 2);
 }
 
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int n;
+	printf("Введите десятичное число: \n");
+	scanf("%d", &n);
+	printf("(Десятичная Cистема счисления): %d = ", n);
+	convertion(n);
+	printf(" (Двоичная система счисления)\n");
+	return 0;
+}

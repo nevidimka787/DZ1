ac1c5d2a93e0e60cdc4fa967bc38f92d5b713ca8
AlexeyVlasoviu4 <91986603+AlexeyVlasoviu4@users.noreply.github.com>
2021-12-02 21:30:44 +0300

    Домашнее задание №2 Власов Алексей ИУ4-31Б

---
+++ dz2.c
@@ -0,0 +1,119 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+    int n;
+    int k = 0;
+    int f = 0;
+    int t = 1;
+    int l = 0;
+    bool flag = false;
+    bool prov = false;
+    scanf_s("%d", &n);
+    int* arr = (int*)malloc(n * n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        printf("%d:", i);
+
+        for (int j = 0; j < n; j++) {
+            scanf_s("%d", (arr + i * n + j));
+        }
+    }
+    printf("\n");
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) {
+
+            if ((i == j) && (arr[i * n + j] != 0)) {    //проверка диагонали
+                prov = true;
+            }
+
+            if (arr[i * n + j] != arr[j * n + i]) {     //проверка симметричности относительно диагонали
+                prov = true;
+            }
+
+            if ((arr[i * n + j] != 0) && (arr[i * n + j] != 1)) {      //проверка либо отсутви€ ребра между вершинами, либо наличи€ лишь 1 таковой
+                prov = true;
+            }
+
+            if (arr[i * n + j] == 1) {      //проверка того, что из каждой вершины выходит ровно 2 ребра
+                k++;
+            }
+
+            if (k > 2) {
+                prov = true;
+            }
+        }
+        k = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) {
+
+            if ((arr[k * n + j] == 1) && (j != t) && (flag == false)) {     //проверка обходом
+                t = k;
+                k = j;
+                flag = true;
+            }
+        }
+        flag = false;
+
+        if ((k == 0) && (i != n - 1)) {
+            prov = true;
+        }
+
+        if ((i == n - 1) && (k != 0)) {
+            prov = true;
+        }
+    }
+    if (prov == false) {
+        printf("simple cycle");
+    }
+    else
+        printf("not a simple cycle");
+
+ // визуализаци€ //
+
+    FILE* file;
+
+    if ((file = fopen("Dot_file.txt", "w")) == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    fprintf(file, "graph G{\n");
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j <= i; j++) {
+
+            if (arr[i * n + j] != 0) {
+
+                for (int g = 0; g < arr[i * n + j]; g++) {
+                    fprintf(file, "%d -- %d;\n", (i + 1), (j + 1));
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((arr[i * n + j]) == 0) {
+                l++;
+            }
+        }
+        if (l == n) {
+            fprintf(file, "%d;\n", (i + 1));
+        }
+        l = 0;
+    }
+        
+    fprintf(file, "}");
+    fclose(file);
+    system("dot C:\\Users\\User\\Desktop\\јлгоритмы\\MyHomework2V2\\Homework2\\Homework2\\Dot_file.txt -Tbmp -o Dot_file.bmp");
+    system("rundll32 \"%ProgramFiles%\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen C:\\Users\\User\\Desktop\\јлгоритмы\\MyHomework2V2\\Homework2\\Homework2\\Dot_file.bmp");
+    return (0);
+}
